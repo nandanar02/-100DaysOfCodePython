f3dc00767b47ffbeb744d66736e3b7b9f43b62eb
nandanar02 <136004921+nandanar02@users.noreply.github.com>
2023-06-10 10:48:08 +0530

    Update Day1.py

--- Day1.py
+++ Day1.py
@@ -3,3 +3,19 @@ print("Day 1 - Python Print Function")
 print("The function is declared like this:")
 print("print('what to print')")
 
+#assignment_2 debugging practice
+print("Day 1 - String Manipulation")
+print("String Concatenation is done with the '+' sign.")
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+#assignment_3
+print(len(input("What is your name")))
+
+#assignment_4
+a = input("a: ")
+b = input("b: ")
+print("a: " + b)
+print("b: " + a)
+
+#assignment_5
