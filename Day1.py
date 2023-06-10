0a3baa9cf27957de3b052750da26c4b6e2b36bb1
nandanar02 <136004921+nandanar02@users.noreply.github.com>
2023-06-10 15:31:00 +0530

    Update Day1.py

--- Day1.py
+++ Day1.py
@@ -19,3 +19,16 @@ print("a: " + b)
 print("b: " + a)
 
 #assignment_5
+#1. Create a greeting for your program.
+print("Welcome to the brand name generator\n")
+
+#2. Ask the user for the city that they grew up in.
+name = input("What's the name of the city you grew up in?\n")
+
+#3. Ask the user for the name of a pet.
+pet = input("What is the name of your pet?\n")
+
+#4. Combine the name of their city and pet and show them their band name.
+print("brand name = " + name + ' ' + pet + "\n")
+
+#5. Make sure the input cursor shows on a new line:
