83302218a14bbe9d01fda898157f45c47790b947
nandanar02 <136004921+nandanar02@users.noreply.github.com>
2023-06-11 19:05:52 +0530

    Update Day3.py

--- Day3.py
+++ Day3.py
@@ -1,1 +1,9 @@
 #assignment_1
+number = int(input("Which number do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+if number%2 == 0:
+ print('This is an even number.')
+else:
+ print('This is an odd number.')
